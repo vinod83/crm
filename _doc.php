e2765a7e15ef551534a0502243e2d63f9b801be3
vinod <vinod@gmail.com>
2022-04-17 22:20:02 +0530

    Login & register with bootstrap

---
+++ _doc.php
@@ -0,0 +1,29 @@
+<php
+
+development .
+
+dell@gupta MINGW32 /d/xampp/htdocs/crm
+$ php artisan passport:install
+Encryption keys generated successfully.
+Personal access client created successfully.
+Client ID: 1
+Client secret: dCyEdWPSMN9gpIAPOqqy9YVGLzcZ3bTDDW4hem5j
+Password grant client created successfully.
+Client ID: 2
+Client secret: MY8eAR8XwY3tXKmNsmYOhIrdyZTXKTYAF1NGZl0H
+
+
+
+// Resources list
+https://laravel.com/docs/8.x/controllers#resource-controllers
+
+
+// login, logout, middleware
+https://www.toptal.com/laravel/passport-tutorial-auth-user-access
+
+
+// Login & Register using Auth
+1. composer require laravel/ui
+2. php artisan ui:auth
+3. Add bootstrap css
+4. Add js with jquery
